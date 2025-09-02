2e5ede0992dde9e37b9ea952895f0143232f0125
tanisha <thetanishasikder@gmail.com>
2025-09-02 19:47:53 -0400

    Fixed some errors

--- prediction.py
+++ prediction.py
@@ -2,16 +2,66 @@ import kagglehub
 import tensorflow as tf
 import random
 import numpy as np
-from io import BytesIO
 import matplotlib.pyplot as plt
-import pathlib
-
-#need to find image size and fix environment issues
+import os
+from kaggle.api.kaggle_api_extended import KaggleApi
 
 # Load in dataset and process the file
-path = kagglehub.dataset_download("fatemehmehrparvar/lung-disease")
-data_dir = tf.keras.utils.get_file(origin=path, extract=True)
-data_dir = pathlib.Path(data_dir).with_suffix('')
+num_epochs = 10
+
+path = "fatemehmehrparvar/lung-disease"
+api = KaggleApi()
+api.authenticate()
+api.dataset_download_files(path, path="data", unzip=True)
+data_dir = "data/Lung X-Ray Image"
+
+#Data augmentation to help the model generalize better
+data_augmentation = tf.keras.Sequential([
+    tf.keras.Input(shape=(300, 300, 3)),
+    # Randomly flip some images
+    tf.keras.layers.RandomFlip('horizontal'),
+    # Randomly rotate some images
+    tf.keras.layers.RandomRotation(0.2, fill_mode='nearest'),
+    # Move to the center of the image by a random amount
+    tf.keras.layers.RandomTranslation(0.2, 0.2, fill_mode='nearest'),
+    # Zooming in some images
+    tf.keras.layers.RandomZoom(0.2, fill_mode='nearest')
+])
+
+# The initial train and validation datasets
+train_dataset = tf.keras.utils.image_dataset_from_directory(
+    directory=data_dir,
+    image_size=(300, 300),
+    batch_size=128,
+    label_mode='categorical',
+    validation_split=0.10,
+    subset='training',
+    seed=42
+)
+
+validation_dataset = tf.keras.utils.image_dataset_from_directory(
+    directory=data_dir,
+    image_size=(300, 300),
+    batch_size=128,
+    label_mode='categorical',
+    validation_split=0.10,
+    subset='validation',
+    seed=42
+)
+
+SHUFFLE_BUFFER_SIZE = 1000
+PREFETCH_BUFFER_SIZE = tf.data.AUTOTUNE
+
+# Final train and validation datasets
+train = (train_dataset
+         .cache()
+         .shuffle(SHUFFLE_BUFFER_SIZE)
+         .prefetch(PREFETCH_BUFFER_SIZE))
+
+validation = (validation_dataset
+              .cache()
+              .shuffle(SHUFFLE_BUFFER_SIZE)
+              .prefetch(PREFETCH_BUFFER_SIZE))
 
 # Create the model. It is for multiclass classification
 model = tf.keras.models.Sequential([
@@ -38,19 +88,6 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.Dense(3, activation='softmax')
 ])
 
-#Data augmentation to help the model generalize better
-data_augmentation = tf.keras.Sequential([
-    tf.keras.Input(shape=(150, 150, 3)),
-    # Randomly flip some images
-    tf.keras.layers.RandomFlip('horizontal'),
-    # Randomly rotate some images
-    tf.keras.layers.RandomRotation(0.2, full_mode='nearest'),
-    # Move to the center of the image by a random amount
-    tf.keras.layers.RandomTranslation(0.2, 0.2, fill_mode='nearest'),
-    # Zooming in some images
-    tf.keras.layers.RandomZoom(0.2, fill_mode='nearest')
-])
-
 # Final model has the augmentation and the original model
 final_model = tf.keras.models.Sequential([
     data_augmentation,
@@ -61,38 +98,4 @@ final_model.compile(loss='categorical_crossentropy',
               optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001),
               metrics=['accuracy'])
 
-# The initial train and validation datasets
-train_dataset = tf.keras.utils.image_dataset_from_directory(
-    directory=data_dir,
-    image_size=(150, 150),
-    batch_size=128,
-    label_mode='categorical',
-    validation_split=0.10,
-    subset='training',
-    seed=42
-)
-
-validation_dataset = tf.keras.utils.image_dataset_from_directory(
-    directory=data_dir,
-    image_size=(150, 150),
-    batch_size=128,
-    label_mode='categorical',
-    validation_split=0.10,
-    subset='validation',
-    seed=42
-)
-
-SHUFFLE_BUFFER_SIZE = 1000
-PREFETCH_BUFFER_SIZE = tf.data.AUTOTUNE
-
-# Final train and validation datasets
-train = (train_dataset
-         .cache()
-         .shuffle(SHUFFLE_BUFFER_SIZE)
-         .prefetch(PREFETCH_BUFFER_SIZE))
-
-validation = (validation_dataset
-              .cache()
-              .shuffle(SHUFFLE_BUFFER_SIZE)
-              .prefetch(PREFETCH_BUFFER_SIZE))
-
+final_model.fit(train, epochs=num_epochs, validation_data=validation, verbose=2)
